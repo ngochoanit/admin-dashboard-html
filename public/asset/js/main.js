373fa32e314b0745c70a79c2c468c96a1b6b2a13
ngochoanit <phamngochoan.it@gmail.com>
2021-10-23 21:52:48 +0700

    admin dashboard

---
+++ public/asset/js/main.js
@@ -0,0 +1,24 @@
+
+// menu active link
+let list = document.querySelectorAll('.navigation li')
+
+function activelink() {
+    list.forEach((item) => {
+        item.classList.remove('hovered')
+        this.classList.add('hovered')
+    })
+}
+
+list.forEach((item) => {
+    item.addEventListener('mouseover', activelink)
+})
+
+// toggle menu
+let toggle = document.querySelector('.toggle')
+let navigation = document.querySelector('.navigation')
+let main = document.querySelector('.main')
+
+toggle.onclick = function () {
+    navigation.classList.toggle('active')
+    main.classList.toggle('active')
+}
